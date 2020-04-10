9a09c93a02575f90c321307b274366b8b980b997
Hamza Tokuchi <h@mza.jp>
2020-04-11 00:25:28 +0200

    test cases

--- bin/node-auction/pallets/auction/src/tests.rs
+++ bin/node-auction/pallets/auction/src/tests.rs
@@ -1,16 +1,19 @@
 // Creating mock runtime here
 
 use crate::*;
-use frame_support::{assert_ok, assert_err, impl_outer_origin, impl_outer_event, parameter_types};
+use frame_support::{
+    assert_err, assert_ok, impl_outer_event, impl_outer_origin, parameter_types,
+    traits::BalanceStatus,
+};
 use frame_system::{self as system, RawOrigin};
-use sp_core::H256;
-use sp_io;
+use pallet_balances::{self as balances};
+use sp_core::{LogLevel, H256};
+use sp_io::{self as io, logging::log};
 use sp_runtime::{
     testing::Header,
     traits::{BlakeTwo256, IdentityLookup},
     Perbill,
 };
-use pallet_balances::{self as balances};
 
 impl_outer_origin! {
     pub enum Origin for AuctionTestRuntime {}
@@ -90,7 +93,7 @@ pub fn new_test_ext() -> sp_io::TestExternalities {
         .build_storage::<AuctionTestRuntime>()
         .unwrap();
     balances::GenesisConfig::<AuctionTestRuntime> {
-        balances: vec![(1, 1300), (2, 1100), (3, 100), (4, 300), (5, 1900)],
+        balances: vec![(1, 20000), (2, 20000), (3, 20000), (4, 20000), (5, 20000)],
     }
     .assimilate_storage(&mut t)
     .unwrap();
@@ -98,34 +101,81 @@ pub fn new_test_ext() -> sp_io::TestExternalities {
     t.into()
 }
 
-
-
 #[test]
 fn new_test_ext_behaves() {
     new_test_ext().execute_with(|| {
-        assert_eq!(Balances::free_balance(&1), 1300);
+        assert_eq!(Balances::free_balance(&1), 20000);
     })
 }
 
 // Currency
 /////////////////////////////////////////////////////////
 #[test]
-fn new_test_transfer() {}
+fn new_test_ext_transfer() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(Balances::transfer(Origin::signed(1), 2, 1000));
+        assert_eq!(Balances::free_balance(&2), 21000);
+        assert_ok!(Balances::transfer(Origin::signed(2), 1, 1000));
+        assert_eq!(Balances::free_balance(&1), 20000);
+    })
+}
 /////////////////////////////////////////////////////////
 // Reservable
 /////////////////////////////////////////////////////////
 #[test]
-fn new_test_can_reserve() {}
+fn new_test_ext_can_reserve() {
+    new_test_ext().execute_with(|| {
+        assert_eq!(Balances::can_reserve(&1, 29), true);
+        assert_eq!(Balances::can_reserve(&1, 20001), false);
+    })
+}
 
 #[test]
-fn new_test_reserve() {}
+fn new_test_ext_reserve() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(Balances::reserve(&1, 10000));
+        assert_eq!(Balances::free_balance(&1), 10000);
+        assert!(Balances::reserve(&1, 31000).is_err());
+    })
+}
+//https://substrate.dev/rustdocs/master/src/frame_support/traits.rs.html#725-783
+#[test]
+fn new_test_ext_unreserver() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(Balances::reserve(&1, 10000));
+        assert_eq!(Balances::free_balance(&1), 10000);
+        assert_eq!(Balances::unreserve(&1, 10000), 0);
+        assert_ok!(Balances::reserve(&1, 20000));
+    })
+}
 
 #[test]
-fn new_test_unreserver() {}
+fn new_test_ext_slash_reserve() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(Balances::reserve(&1, 10000));
+        assert_eq!(Balances::free_balance(&1), 10000);
+        assert_eq!(Balances::reserved_balance(&1), 10000);
+        let slash_res = Balances::slash_reserved(&1, 10000);
+        assert_eq!(Balances::reserved_balance(&1), 0);
+        assert_eq!(Balances::free_balance(&1), 10000);
+    })
+}
 
 #[test]
-fn new_test_slash_reserve() {}
-
-#[test]
-fn new_test_repatriate_reserved() {}
+fn new_test_ext_repatriate_reserved() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(Balances::reserve(&1, 10000));
+        assert_eq!(Balances::free_balance(&1), 10000);
+        assert_eq!(Balances::reserved_balance(&1), 10000);
+        assert_ok!(Balances::repatriate_reserved(
+            &1,
+            &2,
+            Balances::reserved_balance(&1),
+            BalanceStatus::Free
+        ));
+        assert_eq!(Balances::reserved_balance(&1), 0);
+        assert_eq!(Balances::free_balance(&1), 10000);
+        assert_eq!(Balances::free_balance(&2), 30000);
+    })
+}
 ///////////////////////////////////////////////////////
